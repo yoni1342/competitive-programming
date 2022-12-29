bc4f1d389a5e62389339ffed3598977794754c2b
Yonatan Tesfaye <65813507+yoni1342@users.noreply.github.com>
2022-12-29 01:13:55 -0800

    Update 0609-find-duplicate-file-in-system.py

--- 0609-find-duplicate-file-in-system/0609-find-duplicate-file-in-system.py
+++ 0609-find-duplicate-file-in-system/0609-find-duplicate-file-in-system.py
@@ -2,6 +2,9 @@ class Solution:
     def findDuplicate(self, paths: List[str]) -> List[List[str]]:
         count = defaultdict(list)
         
+#       splite all Path with space and the first one is the directory path
+#       after spliting we search the content name and we store the content name as key and the file path 
+#       as value with list in dictonary 
         for path in paths:
             destructure = path.split()
             directory = destructure[0]
@@ -11,7 +14,7 @@ class Solution:
                 content = destructure[fileptr].split('(')[1]
                 content = content[0:len(content)-1]
                 count[content].append(directory+'/'+file)
-        
+#       after we sore in dict then we'll  create answer list that contain only elements that has two or more values or file path 
         answer = [x for x in count.values() if len(x) > 1]
         
         return answer
