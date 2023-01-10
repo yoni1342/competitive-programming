fdabd3955e659ff40931a08fc01a6e8c9bbfcd38
Yonatan Tesfaye <65813507+yoni1342@users.noreply.github.com>
2023-01-10 01:07:30 -0800

    Time: 82 ms (84.34%), Space: 14.6 MB (6.84%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,14 +1,39 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        ans = [0]*(len(boxes))
+        leftOperations = [0]*len(boxes)
+        rightOperations = [0]*len(boxes)
+        leftCount = 0
+        rightCount = 0
         
-        ptr = 0
-        while ptr<len(boxes):
-            c = 0
-            while c<len(boxes):
-                if boxes[c] == '1':
-                    ans[ptr] += abs(ptr-c)
-                c+=1
-            ptr+=1
+        # perform left operations
+        for index,val in enumerate(boxes):
+            if index == 0:
+                leftOperations[index] = 0
+                if val == '1':
+                    leftCount+=1
+            elif val == "1":
+                leftOperations[index] = leftOperations[index-1]+leftCount
+                leftCount+=1
+            else:
+                leftOperations[index] = leftOperations[index-1]+leftCount
         
-        return ans
+        # performing right operations
+        
+        for index, val in reversed(list(enumerate(boxes))):
+            if index == len(boxes)-1:
+                rightOperations[index] = 0
+                if val == '1':
+                    rightCount+=1
+            elif val == '1':
+                rightOperations[index] = rightOperations[index+1] + rightCount
+                rightCount+=1
+            else:
+                rightOperations[index] = rightOperations[index+1]+rightCount
+        
+        # finally add leftOperations and rightOperations
+        
+        answer = [0]*len(boxes)
+        for i in range(len(boxes)):
+            answer[i] = leftOperations[i]+rightOperations[i]
+        
+        return answer
