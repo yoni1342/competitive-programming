a304092893e1bfa0fce5fefe011650c9f86a6c4f
Yonatan Tesfaye <65813507+yoni1342@users.noreply.github.com>
2023-02-01 07:54:50 -0800

    Time: 22 ms (99.41%), Space: 13.9 MB (70.81%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,30 +1,14 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         
-        def divs(prefix, s1, s2):
-            for i in range(len(prefix), len(s1)+1,len(prefix)):
-                if s1[i-len(prefix):i] != prefix:
-                    return False
-            if i<len(s1) and s1[i:] != prefix:
-                return False
-            for i in range(len(prefix), len(s2)+1, len(prefix)):
-                if s2[i-len(prefix):i] != prefix:
-                    return False
-            if i <len(s2) and s2[i:] != prefix:
-                    return False
-            
-            return True
-        prefix = ""
-        if len(str1)>len(str2):
-            prefix = str2
-        elif len(str1)<len(str2):
-            prefix = str1
+        def gcd(a,b):
+            if b==0:
+                return a
+            else:
+                return gcd(b,a%b)
+        
+        if str1+str2 == str2+str1:
+            gcd = str1[:gcd(len(str1), len(str2))]
+            return gcd
         else:
-            return str1 if str1==str2 else "" 
-        
-        ans = ""
-        for i in range(len(prefix)):
-            if divs(prefix[0:i+1], str1, str2):
-                ans = prefix[0:i+1]
-        
-        return ans
+            return ""
