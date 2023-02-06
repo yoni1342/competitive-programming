07551a756122bb25bc292f0461073d073e24521d
Yonatan Tesfaye <65813507+yoni1342@users.noreply.github.com>
2023-02-05 19:30:43 -0800

    Time: 61 ms (79.49%), Space: 14.1 MB (33.52%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        left = 0
+        right = n
+        ans = []
+        while right<len(nums):
+            ans.append(nums[left])            
+            ans.append(nums[right])
+            
+            left+=1
+            right+=1
+        
+        return ans
