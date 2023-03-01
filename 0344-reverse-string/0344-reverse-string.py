277e087536290d97794cd300ba2a817b9abaedb5
Yonatan Tesfaye <65813507+yoni1342@users.noreply.github.com>
2023-03-01 00:48:25 -0800

    Time: 217 ms (25.59%), Space: 43.1 MB (5.48%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,4 +3,14 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        return s.reverse()
+        p1 = 0
+        p2 = len(s)-1
+        self.reverse(p1,p2,s)
+        return s
+    def reverse(self,p1, p2,s):
+        if p1>=p2:
+            return
+        s[p1], s[p2] = s[p2],s[p1]
+        self.reverse(p1+1, p2-1,s)
+        
+        
