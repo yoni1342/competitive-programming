8225d38705fc6569c79166e981c50e8519c88a72
Yonatan Tesfaye <65813507+yoni1342@users.noreply.github.com>
2023-03-03 00:58:47 -0800

    Time: 38 ms (72.47%), Space: 13.8 MB (46.90%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        return int(sqrt(x))
+#         left = 1
+#         right = x//2
+        
+#         while left<=right:
+#             mid = left+(right-left)//2
+            
+#             mid*mid
+        
