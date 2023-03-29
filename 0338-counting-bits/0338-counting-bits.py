2f3e3cbb5dfd0b031a40d8239a7425ea75df8ff5
Yonatan Tesfaye <65813507+yoni1342@users.noreply.github.com>
2023-03-29 19:35:37 +0300

    Time: 199 ms (11.55%), Space: 20.8 MB (19.07%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -4,13 +4,11 @@ class Solution:
         
         for i in range(n+1):
             count = 0
-            bit = i
-            while bit:
-                if bit & 1 != 0:
-                    bit = bit >> 1
-                    count+=1
-                else:
-                    bit = bit >> 1
-            ans.append(count)
+            one = 1
             
+            while one <= i:
+                if one & i != 0:
+                    count+=1
+                one = one << 1    
+            ans.append(count) 
         return ans
