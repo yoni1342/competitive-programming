2822906396457922475f196efc86833f706df80a
Yonatan Tesfaye <65813507+yoni1342@users.noreply.github.com>
2023-04-05 15:01:46 +0300

    Time: 943 ms (53.57%), Space: 17.3 MB (46.43%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        Hash = {}
+        ans = 0
+        
+        for i,val in enumerate(words):
+            res = 0
+            for j in val:
+                mask = 1 << ord(j)-96
+                res |= mask
+            Hash[i] = res
+        
+        for i in Hash:
+            for j in Hash:
+                
+                if Hash[i]&Hash[j] == 0:
+                    ans = max(ans,len(words[i]) * len(words[j]))
+        
+        return ans
