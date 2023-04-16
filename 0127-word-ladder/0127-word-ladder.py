94ef68efc154bd0971647c47575e4305077e3d2e
Yonatan Tesfaye <65813507+yoni1342@users.noreply.github.com>
2023-04-16 21:45:56 +0300

    Time: 710 ms (16.84%), Space: 65.8 MB (5.09%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.py
@@ -0,0 +1,28 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordList = set(wordList)
+        if endWord not in wordList:
+            return 0
+        
+        que = deque([(beginWord, 1)])
+        
+        visited = set([beginWord])
+        
+        while que:
+            word, level = que.popleft()
+            
+            for i in range(len(word)):
+                for letter in 'acbdefghijklmnopqrstuvwxyz':
+                    if letter == word[i]:
+                        continue
+                    
+                    newWord = word[:i] + letter + word[i+1:]
+                    # if newWord in visited:
+                    #     break
+                    if newWord in wordList and newWord not in visited:
+                        if newWord == endWord:
+                            return level+1
+                    
+                        que.append((newWord, level+1))
+                    visited.add(newWord)
+        return 0
