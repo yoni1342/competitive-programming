a977e67e4470f23e9b05aa904ff5f452fdb66d79
Yonatan Tesfaye <65813507+yoni1342@users.noreply.github.com>
2023-05-09 19:03:18 +0300

    Time: 115 ms (23.81%), Space: 19.3 MB (8.63%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,31 +1,38 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
-        income = [0]*numCourses
-        order = []
+        # white = 0, gray = 1, black = 2
+        color = [0]*numCourses
+        ans = []
         
         for i in prerequisites:
-            income[i[0]]+=1
             graph[i[1]].append(i[0])
         
-        que = deque()
-        
-        for i in range(len(income)):
-            if income[i] == 0:
-                que.append(i)
-        
-        while que:
-            course = que.popleft()
-            order.append(course)
+        def dfs(node):
             
-            for neighbor in graph[course]:
-                income[neighbor]-=1
+            color[node] = 1
+            
+            for curnode in graph[node]:
+                col = color[curnode]
                 
-                if income[neighbor] == 0:
-                    que.append(neighbor)
+                if col == 0:
+                    if not dfs(curnode):
+                        return False
+                elif col == 1:
+                    return False
+                else:
+                    continue
+                    
+            color[node] = 2
+            ans.append(node)
+            return True
+
+        for node in range(numCourses):
             
-        if len(order) != numCourses:
-            return []
-        
-        return order
+            if color[node] != 0:
+                continue
             
+            if not dfs(node):
+                return []
+            
+        return ans[::-1]
