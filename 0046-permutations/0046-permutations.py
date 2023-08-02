90e6b29280da0aec9d6a7573e64b0893ac768acf
Yonatan Tesfaye <65813507+yoni1342@users.noreply.github.com>
2023-08-03 00:06:08 +0300

    Time: 53 ms (66.80%), Space: 16.6 MB (41.50%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,22 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        path = []
-        tester = 0
+        prev = set()
         
-        def back(idx):
-            nonlocal tester
-            if idx == len(nums):
+        def back(path):
+            if len(path) == len(nums):
                 ans.append(path[:])
-                return 
+                return;
             
             for i in range(len(nums)):
-                if (1<<i) & tester == 0:
-                    tester |= 1<<i
+                if nums[i] not in prev:
                     path.append(nums[i])
-                    back(idx+1)
-                    path.pop()
-                    tester &= ~(1<<i)
-        
-        back(0)
+                    prev.add(nums[i])
+                    back(path)
+                    prev.remove(path.pop())
+                    
+        back([])
         return ans
