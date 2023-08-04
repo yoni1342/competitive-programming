ca2eb0bf3650dc723e0f7a3d89508892992b1ab1
Yonatan Tesfaye <65813507+yoni1342@users.noreply.github.com>
2023-08-05 00:31:13 +0300

    Time: 43 ms (92.07%), Space: 16.5 MB (60.68%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        q = deque([s])
+        seen = set() 
+        while q:
+            s = q.popleft()
+            for word in wordDict:
+                if s.startswith(word):
+                    new_s = s[len(word):]
+                    if new_s == "": 
+                        return True
+                    if new_s not in seen:
+                        q.append(new_s)
+                        seen.add(new_s)
+        return False
